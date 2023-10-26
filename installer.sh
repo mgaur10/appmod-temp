f531dded6aa5c244e172eb3d605910a19824a739
Will Sulzer <willsulzer@google.com>
2023-10-26 16:24:51 +0000

    install nc in cloud shell

--- installer.sh
+++ installer.sh
@@ -41,6 +41,9 @@ PROJECT_HOME="${WORKSTATION_USER_HOME}/${USER_SOURCE_REPO_NAME}"
 WORK_DIR="$(mktemp -d)/${USER_SOURCE_REPO_NAME}"
 cp -r java-sample-app "${WORK_DIR}"
 
+# Install 'nc' if it doesn't already exits
+sudo apt-get install netcat
+
 # Don't substitute $PROJECT_ID in cloudbuild.yaml since it's a predefined Cloud Build var
 envsubst '$DELIVERY_PIPELINE_NAME $DOCKER_REPO_NAME $PRIVATE_WORKER_POOL $ATTESTOR_NAME $KMS_KEYRING_NAME $KMS_KEY_NAME $KMS_KEY_VERSION $SERVICE_ACCOUNT' < "${WORK_DIR}/cloudbuild.yaml.tmpl" > "${WORK_DIR}/cloudbuild.yaml"
 rm "${WORK_DIR}/cloudbuild.yaml.tmpl"
