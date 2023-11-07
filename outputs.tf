455aaae64cbd617c576ae1d4a4dfb11043efb835
mgaur10 <46466824+mgaur10@users.noreply.github.com>
2023-11-07 12:23:01 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -16,31 +16,6 @@
 ##  This code creates demo environment for CSA - Application Development Architecture Pattern   ##
 ##  This demo code is not built for production workload ##
 
-/*
-output "developer_service_account" {
-  value = module.app-dev.developer_service_account
-}
-
-
-output "default_service_account" {
-  value = module.app-dev.default_service_account
-}
-
-output "ssh_workstation" {
-  value = module.app-dev.workstation_ssh_access
-}
-
-
-output "workstation_https_access" {
-  value = module.app-dev.workstation_https_access
-}
-
-
-/*
-output "workstation_all_attribute" {
-  value = module.app-dev.workstation_all_attribute
-}
-*/
 
 output "_step1_follow_the_steps_to_register_service_account_for_Assured_OSS" {
   value     = module.app-dev._step1_follow_the_steps_to_register_service_account_for_OSS
@@ -48,9 +23,4 @@ output "_step1_follow_the_steps_to_register_service_account_for_Assured_OSS" {
 
 output "_step2_register_the_service_account_for_Assured_OSS" {
   value     = module.app-dev._step2_register_the_service_account_for_OSS
-}
-
-output "gke_cluster" {
-  value     = module.app-dev.gke_cluster
-  sensitive = true
 }
