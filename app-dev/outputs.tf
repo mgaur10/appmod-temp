860d4c5083bd955fcc42b7af94056ccb1d832dd3
mgaur10 <46466824+mgaur10@users.noreply.github.com>
2023-11-08 16:46:38 -0600

    Update outputs.tf

--- app-dev/outputs.tf
+++ app-dev/outputs.tf
@@ -15,33 +15,7 @@
 
 ##  This code creates demo environment for CSA - Application Development Architecture Pattern   ##
 ##  This demo code is not built for production workload ##
-/*
-output "developer_service_account" {
-  value = "${google_service_account.developer_service_account.email}"
-}
 
-
-output "default_service_account" {
-  value = "${google_project.app_dev_project.number}-compute@developer.gserviceaccount.com"
-}
-
-output "workstation_ssh_access" {
-  value = "gcloud beta workstations ssh --project=${google_project.app_dev_project.project_id} --cluster=${google_workstations_workstation_cluster.workstation_cluster.workstation_cluster_id} --config=${google_workstations_workstation_config.workstation_cluster_config_2.workstation_config_id} --region=${var.network_region} ${google_workstations_workstation.hello_world_workstation.workstation_id}"
-}
-
-
-output "workstation_https_access" {
-  value = "https://${google_workstations_workstation.hello_world_workstation.host}"
-}
-
-
-
-output "workstation_all_attribute" {
-  value = "gcloud beta workstations ssh --project=${google_project.app_dev_project.project_id} --cluster=${google_workstations_workstation_cluster.workstation_cluster.workstation_cluster_id} --config=${google_workstations_workstation_config.workstation_cluster_config_2.workstation_config_id} --region=${var.network_region} ${google_workstations_workstation.hello_world_workstation.workstation_id} -- 'gcloud init && gcloud source repos create hello-world-java && gcloud source repos clone hello-world-java && git config --global user.email 'admin@manishkgaur.altostrat.com' && git config --global user.name 'Manish Gaur' && gcloud source repos clone springboard-java-hello-world --project springboard-dev-env && pushd . && cd springboard-java-hello-world && git checkout main && popd && cp -r springboard-java-hello-world/* springboard-java-hello-world/.mvn springboard-java-hello-world/.gitignore hello-world-java/ && cd hello-world-java && git add . && git commit -m 'first commit' && ./installer.sh && export PROJECT_HOME=/home/user/hello-world-java && export SERVICE_ACCOUNT=${google_service_account.developer_service_account.email} && source ~/.hello-world-dev-config $SERVICE_ACCOUNT $PROJECT_HOME'"
-}
-
-
-*/
 
 output "_step1_follow_the_steps_to_register_service_account_for_OSS" {
   value     = "Follow the URL: https://assuredoss.developers.google.com/"
@@ -51,5 +25,17 @@ output "_step1_follow_the_steps_to_register_service_account_for_OSS" {
 output "_step2_register_the_service_account_for_OSS" {
   value     = "${google_service_account.developer_service_account.email}"
 }
+
+output "_step3_execute_the_command_toget_workstation_HTTP_URL" {
+  value = "terraform apply --refresh-only"
+}
+
+output "_step4_workstation_https_access" {
+  value = "https://${google_workstations_workstation.hello_world_workstation.host}"
+}
+
+
+
+
 
 
